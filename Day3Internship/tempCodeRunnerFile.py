f567775cf3b15e2404ca2a61d0fe16300cbc2f35
shettyvarshaa <shettyvarshaa275@gmail.com>
2023-10-16 11:01:51 +0530

    Deleted DAy2TAsk1

--- Day3Internship/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-new_df = df.dropna()
-# print(new_df.to_string())
-# #OR
-# df.dropna(inplace = True) #Remove all rows with NULL values
-# print(df.to_string())
-# #OR
-# df.fillna(130, inplace = True) #Replace NULL values with the number 130
-# print(df.to_string())
